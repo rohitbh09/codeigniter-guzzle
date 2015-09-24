bb12f2d27a063bdb2e4c254b17c00e13aa00312f
rohit <rohit@webdev.NASA>
2015-09-24 10:34:35 +0530

    guzzel file addded

---
+++ guzzel.php
@@ -0,0 +1,10 @@
+<?php
+  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+  class Voitekk_Guzzel {
+    public function Voitekk_Guzzel() {
+      // require_once('vendor/autoload.php');
+      require 'vendor/autoload.php';
+    }
+  }
+?>  
